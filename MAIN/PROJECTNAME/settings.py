3f43709ba7ea8b295ad2e8a7b20aa21f7ebfab0c
SamiAProjektyWSB <sami.projektyezn@gmail.com>
2023-05-06 18:37:36 +0200

    s

--- MAIN/PROJECTNAME/settings.py
+++ MAIN/PROJECTNAME/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+#import django_heroku
 import dj_database_url
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
