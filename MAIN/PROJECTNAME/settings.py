22d2e946c8ace5d7ade0347686ae3df2460a9e84
SamiAProjektyWSB <sami.projektyezn@gmail.com>
2023-05-06 18:50:22 +0200

    poprawki

--- MAIN/PROJECTNAME/settings.py
+++ MAIN/PROJECTNAME/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import django_heroku
 #import dj_database_url
 
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +135,7 @@ if not DEBUG:
 else:
     STATICFILES_DIRS=[BASE_DIR / 'static']
 
-
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
